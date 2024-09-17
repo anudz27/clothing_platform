d8b0d0b3ccc4ac68296c60c3ef248592a6f2841b
Shashini Dias <117101873+anudz27@users.noreply.github.com>
2024-09-18 00:01:11 +0530

    second file  commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,7 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { i } from 'vite/dist/node/types.d-aGj9QkWt'
 
 // https://vitejs.dev/config/
 export default defineConfig({
